0ec513558f8533f5b558f4bfc4093221c3108fc1
Marshall Brandt <MarshallCB@users.noreply.github.com>
2020-10-23 22:36:54 -0500

    jk, method from earlier is better, reverting

--- src/Model.js
+++ src/Model.js
@@ -1,7 +1,5 @@
 import { iterate } from './utils'
 
-let { defineProperty, getOwnPropertyDescriptor, seal, assign } = Object
-
 function parseDefinition(d,x){
   let impn = [()=>{},[],[],[]]
   Object.getOwnPropertyNames(d)
@@ -16,112 +14,112 @@ function parseDefinition(d,x){
   return impn
 }
 
-function define(definition,initial={}){
-  function Ctex() {}
-  const {prototype} = Ctex;
-  function def(key,value,custom=false){
-    defineProperty(prototype,key,custom?value:{value})
+// TODO: try replacing Map() with {}
+// TODO: generalize flattie to improve values() and set() for arrays and objects
+// TODO: include flag for whether root subscription should trigger save (no if from internal node)
+
+class Ctex{
+  constructor(impn,definition,initial){
+    // this.init = impn[0].bind(this)
+    let def = function(key,value,custom=false){
+      Object.defineProperty(this,key,custom?value:{value})
+    }.bind(this)
+    // marker
+    def('_isCtex',true)
+    // subscribers
+    def('s',{})
+    // succinct way to define generator function to iterate over properties and nodes
+    // used by the iterate function in utils
+    def(Symbol.iterator,{*a(){yield* impn[2];yield* impn[3]}}.a)
+    
+    // Set methods
+    impn[1].map(k => {
+      def(k,{
+        set(x){ Promise.resolve(definition[k](x)) },
+        get(){ return definition[k] },
+        enumerable: true
+      },true)
+    })
+    // Set properties
+    impn[2].map(k => {
+      let { get, set } = Object.getOwnPropertyDescriptor(definition,k)
+      def('_$'+k,{writable:true},true)
+      def(k,get ? {get} : {
+        set(x){
+          x = set ? set(x) : x
+          if(this['_$'+k] !== x){
+            this['_$'+k] = x;
+            let s = this.s
+            if(s[k]){
+              for(let f of s[k])
+                Promise.resolve(f(x))
+            }
+            if(s['']){
+              let vals = iterate(this)
+              for(let f of s[''])
+                Promise.resolve(f(vals))
+            }
+          }
+        },
+        get(){
+          return this['_$'+k]
+        },
+        enumerable: true
+      },true)
+      if(!get)
+        this[k] = initial[k] || definition[k]
+    })
+    // set inner nodes
+    impn[3].map(k => {
+      def(k,{
+        set(x={}){
+          if(this[k] instanceof Ctex){
+            this[k].set(x)
+          } else {
+            this[k] = definition[k](x)
+          }
+        },
+        enumerable: true
+      },true)
+      this[k] = initial[k]
+    })
+    Object.seal(this)
+    // call init function
+    impn[0].bind(this)()
   }
-
-  let impn = parseDefinition(definition)
-  def('s',{})
-  def(Symbol.iterator,{*a(){yield* impn[2];yield* impn[3]}}.a)
-  // Set methods
-  impn[1].map(k => {
-    def(k,{
-      set(x){ Promise.resolve(definition[k](x)) },
-      get(){ return definition[k] },
-      enumerable: true
-    },true)
-  })
-  // Set properties
-  impn[2].map(k => {
-    let { get, set } = getOwnPropertyDescriptor(definition,k)
-    def('_$'+k,{writable:true},true)
-    def(k,get ? {get} : {
-      set(x){
-        x = set ? set(x) : x
-        if(prototype['_$'+k] !== x){
-          prototype['_$'+k] = x;
-          let s = prototype.s
-          if(s[k]){
-            for(let f of s[k])
-              Promise.resolve(f(x))
-          }
-          if(s['']){
-            let vals = iterate(prototype)
-            for(let f of s[''])
-              Promise.resolve(f(vals))
-          }
-        }
-      },
-      get(){
-        return prototype['_$'+k]
-      },
-      enumerable: true
-    },true)
-    if(!get)
-      prototype[k] = initial[k] || definition[k]
-  })
-  // set inner nodes
-  impn[3].map(k => {
-    def(k,{
-      set(x={}){
-        if(prototype[k] instanceof Ctex){
-          prototype[k].set(x)
-        } else {
-          prototype[k] = definition[k](x)
-        }
-      },
-      enumerable: true
-    },true)
-    prototype[k] = initial[k]
-  })
-  // set built-in methods
-
-  assign(prototype,{
-    subscribe(k,fn,id=Symbol()){
-      if(this[k] !== undefined){
-        // if only function is passed, assume they're subscribing to root function
-        if(!fn){
-          fn = k
-          k = ''
-        }
-        fn=fn.bind(this)
-        if(this[k] && this[k]._isCtex){
-          // subscribe to context's root
-          return this[k].subscribe(fn)
-        }
-        this.s[k] = this.s[k]||new Set()
-        this.s[k].add(fn)
-        return ()=>this.s[k].delete(fn)
+  subscribe(k,fn,id=Symbol()){
+    if(this[k] !== undefined){
+      // if only function is passed, assume they're subscribing to root function
+      if(!fn){
+        fn = k
+        k = ''
       }
-    },
-    set(obj={}){
-      assign(this,obj)
-    },
-    values(){
-      return iterate(this)
+      fn=fn.bind(this)
+      if(this[k] && this[k]._isCtex){
+        // subscribe to context's root
+        return this[k].subscribe(fn)
+      }
+      this.s[k] = this.s[k]||new Set()
+      this.s[k].add(fn)
+      return ()=>this.s[k].delete(fn)
     }
-  })
-
-  def('constructor', Ctex)
-  def('_isCtex', true)
-  seal(prototype)
-  impn[0].bind(prototype)()
-
-  return Ctex;
+  }
+  set(obj={}, recurseFlag=true){
+    Object.assign(this,obj)
+  }
+  values(){
+    return iterate(this)
+  }
 }
 
 function Model(definition){
-  let fn = (initial) => new (define(definition,initial))()
+  let fn = (initial) => Context(definition,initial)
   fn._isCtex = true;
   return fn;
 }
+
 function Context(definition,initial={}){
-  // return new Ctex(parseDefinition(definition),definition,initial)
-  let c = define(definition, initial)
-  return new c();
+  return new Ctex(parseDefinition(definition),definition,initial)
 }
 
 export { Model, Context }
