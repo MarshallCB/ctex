889a78da6ca409f43a6a4d3e881c9ffe3df69b74
Marshall Brandt <MarshallCB@users.noreply.github.com>
2020-10-24 22:23:23 -0500

    more refactors

--- dist/index.mjs
+++ dist/index.mjs
@@ -43,7 +43,7 @@ function traverse(obj, key, p, undef) {
 function parseDefinition(d,x){
   let impn = [()=>{},[],[],[]];
   Object.getOwnPropertyNames(d)
-  .forEach(p => {
+  .map(p => {
     x=d[p];
     if(p !== 'init'){
       impn[typeof x === 'function' ? 1 : (x && x._isCtex) ? 3 : 2].push(p);
@@ -55,22 +55,28 @@ function parseDefinition(d,x){
 }
 
 class Ctex{
-  constructor(impn,definition,initial){
-    // this.init = impn[0].bind(this)
-    let def = function(key,value,custom=false){
+  /**
+   * impn: array of form [init,methods,properties,nodes]
+   * definition: original Model / Context definition schema
+   * initial: possible initial values
+   */
+  constructor(impn,definition,initial={}){
+    // shortcut function to define property (with different defaults than this[key] assignments)
+    let defineProp = function(key,value,custom){
       Object.defineProperty(this,key,custom?value:{value});
     }.bind(this);
-    // marker
-    def('_isCtex',true);
-    // subscribers
-    def('s',{});
+    // marker (allow other functions treat us like Ctex instances)
+    defineProp('_isCtex',true);
+    // subscribers (s for brevity)
+    defineProp('s',{});
     // succinct way to define generator function to iterate over properties and nodes
     // used by the iterate function in utils
-    def(Symbol.iterator,{*a(){yield* impn[2];yield* impn[3];}}.a);
+    defineProp(Symbol.iterator,{*a(){yield* impn[2];yield* impn[3];}}.a);
     
     // Set methods
     impn[1].map(k => {
-      def(k,{
+      defineProp(k,{
+        // allows for shorthand calls to methods in generator functions: menu.choose = yield it.choice(menu)
         set(x){ Promise.resolve(definition[k](x)); },
         get(){ return definition[k] },
         enumerable: true
@@ -79,21 +85,25 @@ class Ctex{
     // Set properties
     impn[2].map(k => {
       let { get, set } = Object.getOwnPropertyDescriptor(definition,k);
-      def('_$'+k,{writable:true},true);
-      def(k,get ? {get} : {
+      defineProp('_$'+k,{writable:true},true);
+      defineProp(k,get ? {get} : {
         set(x){
+          // if custom setter function exists, use returned value from set function
           x = set ? set(x) : x;
+          // ensure that this update changed the value (TODO: always flag?)
           if(this['_$'+k] !== x){
             this['_$'+k] = x;
+            // alert subscribers
             let s = this.s;
             if(s[k]){
               for(let f of s[k])
-                Promise.resolve(f(x));
+                f(x,this);
             }
+            // root subscription key is '' to not conflict with any other keys
             if(s['']){
               let vals = iterate(this);
               for(let f of s[''])
-                Promise.resolve(f(vals));
+                f(this);
             }
           }
         },
@@ -102,16 +112,20 @@ class Ctex{
         },
         enumerable: true
       },true);
+      // if a custom getter was set, we don't need values
       if(!get)
+        // set value with default in definition
         this[k] = initial[k] || definition[k];
     });
     // set inner nodes
     impn[3].map(k => {
-      def(k,{
+      defineProp(k,{
+        // setting a node should invoke the .set() method on such context
         set(x={}){
           if(this[k] instanceof Ctex){
             this[k].set(x);
           } else {
+            // if the instance hasn't been invoked yet, invoke the original definition in the definition (TODO: improve)
             this[k] = definition[k](x);
           }
         },
@@ -119,18 +133,22 @@ class Ctex{
       },true);
       this[k] = initial[k];
     });
+    // Prevent object from being modified, but values can be written to
     Object.seal(this);
     // call init function
-    impn[0].bind(this)();
+    impn[0].call(this);
   }
-  subscribe(k,fn,id=Symbol()){
+  // subscribe(key, fn)  => keyed subscription
+  // subscribe(fn) => root subscription (listen for all updates)
+  subscribe(k,fn){
+    // make sure this subscription makes sense
     if(this[k] !== undefined){
-      // if only function is passed, assume they're subscribing to root function
+      // if only function is passed, assume they're subscribing to root function (only 1 arg)
       if(!fn){
         fn = k;
         k = '';
       }
-      fn=fn.bind(this);
+      // check this[k] in case it's null
       if(this[k] && this[k]._isCtex){
         // subscribe to context's root
         return this[k].subscribe(fn)
@@ -149,13 +167,10 @@ class Ctex{
 }
 
 function Model(definition){
-  let fn = (initial) => Context(definition,initial);
+  let impn = parseDefinition(definition);
+  let fn = (initial) => new Ctex(impn,definition,initial);
   fn._isCtex = true;
   return fn;
-}
-
-function Context(definition,initial={}){
-  return new Ctex(parseDefinition(definition),definition,initial)
 }
 
 function Network(def){
